70b27585d7f7ca3e051fcfb9a122e3bfceca1844
Muhammad Taftiyan Ilham Akbar <taftianilham@gmail.com>
2023-12-02 19:03:06 +0700

    Add files via upload

---
+++ desyhanidhiasari/script/function.js
@@ -0,0 +1,13 @@
+console.log("Foo")
+function showSweetAlert() {
+    Swal.fire({
+        title: "Relativitas",
+        text: "Desy Hani Dhiasari yang sedang berulang tahun, bersama surat elektornik ini, kuberikan",
+        icon: "question"
+      });
+  }
+
+//   function showText() {
+//     console.log("Foo")
+//     alert("Test")
+//   }
